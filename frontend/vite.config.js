40670761d8c3301ee5d0ad27fbced6c3e2f619e5
CleversonTi <clevinho_tiago@hotmail.com>
2025-07-25 23:08:52 -0300

    :wrench: chore(vite): adiciona suporte a path alias (@), configura SCSS global e __dirname

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,24 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'crypto.subtle': 'undefined', 
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/components/styles/main.scss";`
+      }
+    }
+  }
 })
